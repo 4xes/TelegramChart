46428adaba312fce1412346263d08cc72b8d28f3
a.petrushin <a.petrushin@corp.mail.ru>
2019-03-15 17:31:05 +0300

    colors for views

--- app/src/main/java/com/telegram/chart/view/base/Theme.java
+++ app/src/main/java/com/telegram/chart/view/base/Theme.java
@@ -16,13 +16,21 @@ public class Theme {
     private int primaryDarkColor;
     private int backgroundWindowColor;
     private int backgroundSecondColor;
+    private int dividerColor;
+    private int axisValueColor;
+    private int axisColor;
+    private int nameColor;
 
-    public Theme(Context context,@ThemeId int themeId, @ColorRes int primaryColor, @ColorRes int primaryDarkColor, @ColorRes int backgroundWindowColor, @ColorRes int backgroundSecondColor) {
-        this.id = themeId;
+    public Theme(Context context, @ThemeId int id, int primaryColor, int primaryDarkColor, int backgroundWindowColor, int backgroundSecondColor, int dividerColor, int axisValueColor, int axisColor, int nameColor) {
+        this.id = id;
         this.primaryColor = ContextCompat.getColor(context, primaryColor);
         this.primaryDarkColor = ContextCompat.getColor(context, primaryDarkColor);
         this.backgroundWindowColor = ContextCompat.getColor(context, backgroundWindowColor);
         this.backgroundSecondColor = ContextCompat.getColor(context, backgroundSecondColor);
+        this.dividerColor = ContextCompat.getColor(context, dividerColor);
+        this.axisValueColor = ContextCompat.getColor(context, axisValueColor);
+        this.axisColor = ContextCompat.getColor(context, axisColor);
+        this.nameColor = ContextCompat.getColor(context, nameColor);
     }
 
     public int getPrimaryColor() {
@@ -41,14 +49,60 @@ public class Theme {
         return backgroundSecondColor;
     }
 
+    public int getDividerColor() {
+        return dividerColor;
+    }
+
+    public int getAxisValueColor() {
+        return axisValueColor;
+    }
+
+    public int getAxisColor() {
+        return axisColor;
+    }
+
+    public int getNameColor() {
+        return nameColor;
+    }
+
     public static Theme createTheme(Context context, @ThemeId int themeId) {
         switch (themeId) {
             case DAY:
-                return new Theme(context, themeId, R.color.primary_day, R.color.primary_dark_day, R.color.background_day, R.color.background_second_day);
+                return createDay(context);
             case NIGHT:
-                return new Theme(context, themeId, R.color.primary_night, R.color.primary_dark_night, R.color.background_night, R.color.background_second_night);
+                return createNight(context);
         }
-        return new Theme(context, themeId, R.color.primary_day, R.color.primary_dark_day, R.color.background_day, R.color.background_second_day);
+        return createDay(context);
+    }
+
+    private static Theme createNight(Context context) {
+        return new Theme(
+                context,
+                DAY,
+                R.color.primary_night,
+                R.color.primary_dark_night,
+                R.color.background_night,
+                R.color.background_second_night,
+                R.color.divider_night,
+                R.color.axis_value_night,
+                R.color.axis_night,
+                R.color.column_name_night
+        );
+    }
+
+    private static Theme createDay(Context context) {
+        return new Theme(
+                context,
+                DAY,
+                R.color.primary_day,
+                R.color.primary_dark_day,
+                R.color.background_day,
+                R.color.background_second_day,
+                R.color.divider_day,
+                R.color.axis_value_day,
+                R.color.axis_day,
+                R.color.column_name_day
+        );
     }
 
     public static final int DAY = 0;
